3f5420ed40dd95392982ed208551b563f6ba0b3c
AJ Beck <aj.beck@healthstream.com>
2019-08-15 14:59:09 -0500

    [minor] (awscli-1.16.218): Update AWS CLI

---
+++ Formula/awscli616a.rb
@@ -0,0 +1,51 @@
+class Awscli616a < Formula
+  include Language::Python::Virtualenv
+
+  desc "Amazon AWS command-line interface"
+  homepage "https://aws.amazon.com/cli/"
+  # awscli should only be updated every 10 releases on multiples of 10
+  url "https://github.com/aws/aws-cli/archive/1.16.218.tar.gz"
+  sha256 "df7646fb144a5a403e4098a8b24dd0cc64c976f9e75dc9719237ccc2e082ef7e"
+  head "https://github.com/aws/aws-cli.git", :branch => "develop"
+
+  bottle do
+    root_url "https://public-fs.616a.io/artifacts/homebrew-taps/awscli/"
+    cellar :any
+    sha256 "fa76727b0dcce73ef04d2532d6269cb791926dbad61b2a2a2aeda19b3caf0593" => :mojave
+  end
+
+  # Some AWS APIs require TLS1.2, which system Python doesn't have before High
+  # Sierra
+  depends_on "python"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
+                              "--ignore-installed", "PyYAML==3.13", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "awscli"
+    venv.pip_install_and_link buildpath
+    pkgshare.install "awscli/examples"
+
+    rm Dir["#{bin}/{aws.cmd,aws_bash_completer,aws_zsh_completer.sh}"]
+    bash_completion.install "bin/aws_bash_completer"
+    zsh_completion.install "bin/aws_zsh_completer.sh"
+    (zsh_completion/"_aws").write <<~EOS
+      #compdef aws
+      _aws () {
+        local e
+        e=$(dirname ${funcsourcetrace[1]%:*})/aws_zsh_completer.sh
+        if [[ -f $e ]]; then source $e; fi
+      }
+    EOS
+  end
+
+  def caveats; <<~EOS
+    The "examples" directory has been installed to:
+      #{HOMEBREW_PREFIX}/share/awscli/examples
+  EOS
+  end
+
+  test do
+    assert_match "topics", shell_output("#{bin}/aws help")
+  end
+end
