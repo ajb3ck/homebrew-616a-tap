5a607290505cc2099598588796d741c7106433a4
AJ Beck <aj.beck@healthstream.com>
2019-11-21 02:04:52 -0600

    update AWS CLI to 1.16.287

--- Formula/awscli616a.rb
+++ Formula/awscli616a.rb
@@ -4,14 +4,14 @@ class Awscli616a < Formula
   desc "Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
   # awscli should only be updated every 10 releases on multiples of 10
-  url "https://github.com/aws/aws-cli/archive/1.16.285.tar.gz"
-  sha256 "7cd8c631d34c44906182e37e60e2ca18c97314dd9231ca975d8340eda4dc4446"
+  url "https://github.com/aws/aws-cli/archive/1.16.287.tar.gz"
+  sha256 "313649b7cebd5908693c9bc8e83a7546d780725a84431b68d442c6a78d8b67bd"
   head "https://github.com/aws/aws-cli.git", :branch => "develop"
 
   bottle do
     root_url "https://public-fs.616a.io/artifacts/homebrew-taps/awscli"
     cellar :any
-    sha256 "bd6a9f838604a722ca9a4fd694d633302975fb36852690d0ed3e7977ba58523d" => :catalina
+    sha256 "9605e7acfcb3dd73179f43c8e8090ff0ed2d3b6ade98d2dd6df5623a1083f635" => :catalina
   end
 
   # Some AWS APIs require TLS1.2, which system Python doesn't have before High
